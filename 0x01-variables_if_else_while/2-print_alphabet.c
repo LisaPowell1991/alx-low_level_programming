d991e2c1e1e21ffb01b6f9a5aa4dbb71ea161598
LisaPowell1991 <lisapowell46@gmail.com>
2023-02-18 04:28:46 -0800

    include ctype.h

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+for (int x = 'A'; x <= 'Z'; x++)
+{
+putchar(tolower(x));
+}
+return (0);
+}
