6d49a6a7377b2e1027557fd9aa4f51924de8b864
LisaPowell1991 <lisapowell46@gmail.com>
2023-02-19 06:08:47 -0800

    different combos of 2digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, i;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+		if (n < i)
+			{
+		putchar(n);
+		putchar(i);
+
+		if (n != '8' || (n == '8' && i != '9'))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
