6db0c4652a04ec891f773571b1c5fb0d83c8bf53
LisaPowell1991 <lisapowell46@gmail.com>
2023-02-21 07:48:01 -0800

    alphabet void

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - _putchar alphabet
+ *
+ * Return: Always void
+ */
+
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
