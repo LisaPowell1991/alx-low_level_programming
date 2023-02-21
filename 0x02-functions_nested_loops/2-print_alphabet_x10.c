50c31a1ac611f71f57302feaee3e12ce03af8c45
LisaPowell1991 <lisapowell46@gmail.com>
2023-02-21 07:56:58 -0800

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "1-alphabet.c"
+
+/**
+ * print_alphabet_x10 - alphabet x10
+ *
+ * Return: Always void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		print_alphabet();
+	}
+}
