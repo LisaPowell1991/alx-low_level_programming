0c7fe5a06c542ed5b58f2ca8fe220cc5fc2d9160
LisaPowell1991 <lisapowell46@gmail.com>
2023-02-22 07:13:08 -0800

    natural nums

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,11 +11,13 @@ int main(void)
 {
 	int sum = 0;
 
-	for (int i = 0; i < 1024; i += 5)
+	int 1;
+
+	for (i = 0; i < 1024; i += 5)
 	{
 		sum += i;
 	}
-	for (int i = 0; i < 1024; i += 3)
+	for (i = 0; i < 1024; i += 3)
 	{
 		if (i % 5)
 			sum += i;
