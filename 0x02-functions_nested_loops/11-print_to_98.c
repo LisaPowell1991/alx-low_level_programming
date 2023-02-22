5f62c06aee506607ad5330846c7faec66e10b1a2
LisaPowell1991 <lisapowell46@gmail.com>
2023-02-21 20:23:41 -0800

    natural nums

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,21 +20,23 @@ void print_to_98(int n)
 				printf(",");
 			}
 			n++;
-			else if (n > 98)
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
 			{
-				while (n >= 98)
-				{
-					printf("%d", n);
-					if (n != 98)
-					{
-						printf(", ");
-					}
-					n--;
-				}
+				printf(", ");
 			}
-			else
-			{
+			n--;
+		}
+	}
+	else
+	{
 		printf("98");
-			}
-		printf("\n");
+	}
+	printf("\n");
 }
