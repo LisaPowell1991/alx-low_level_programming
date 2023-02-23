dacdb194b65253bb16b3deafce793bf47c7bf6ea
LisaPowell1991 <lisapowell46@gmail.com>
2023-02-23 12:28:15 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,34 +8,31 @@
 
 int main(void)
 {
-	int i;
+int i;
 
-	for (i = 1; i <= 100; i++)
+for (i = 1; i <= 100; i++)
+{
+	if (i % 3 == 0 && i % 5 == 0)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 5 == 0)
-		{
-			if (i == 100)
-			{
-				 printf("Buzz");
-			}
-			else
-			{
-				printf("Buzz ");
-			}
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else
-		{
-			printf("%d ", i);
-		}
+		printf("FizzBuzz ");
 	}
-	printf("\n");
-	return (0);
+	else if (i % 5 == 0 && i == 100)
+	{
+		printf("Buzz");
+	}
+	else if (i % 5 == 0)
+	{
+		printf("Buzz ");
+	}
+	else if (i % 3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else
+	{
+		printf("%d ", i);
+	}
+}
+printf("\n");
+return (0);
 }
