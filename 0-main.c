d5bd5481dd9075172a8625f40a8c45ecf2e130c5
LisaPowell1991 <lisapowell46@gmail.com>
2023-02-22 19:57:12 -0800

    positive or negatives

--- 0-main.c
+++ 0-main.c
@@ -10,6 +10,16 @@ int main(void)
 	int i;
 
 	i = 0;
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
 	positive_or_negative(i);
 	return (0);
 }
