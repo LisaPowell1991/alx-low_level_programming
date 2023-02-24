05c03ee38b2635e3898b18fb0e0cc5964e3797e6
LisaPowell1991 <lisapowell46@gmail.com>
2023-02-23 22:13:31 -0800

    print integers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,24 +8,20 @@
 
 void print_number(int n);
 {
-        if (n < 0)
-        {
-                _putchar('-');
-                n = -n;
-        }
+unsigned int k = n;
 
-        if (n == 0)
-        {
-                _putchar('0');
-        }
+if (n < 0)
+{
+n *= -1;
+k = n;
+_putchar('-');
+}
+k /= 10;
 
-        if (n /10)
-        {
-                print_number(n / 10);
-
-                _putchar(n % 10 + '0');
-        }
-        _putchar('\n');
+if (k != 0)
+{
+print_number(k);
 }
 
-     
+_putchar((unsigned int) n % 10 + '0');
+}
