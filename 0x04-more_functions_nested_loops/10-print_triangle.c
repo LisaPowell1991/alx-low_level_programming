5e64d169b3e383dc9e0e866d43f85236b8c37253
LisaPowell1991 <lisapowell46@gmail.com>
2023-02-23 20:18:05 -0800

    prints triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,30 +10,27 @@ include "main.h"
 
 void print_triangle(int size)
 {
-	int a, b;
+	int a, b, c;
 
-	if (size > 0)
+	if (size <= 0)
 	{
-		for (a = 1; a <= size; a++)
+		_putchar('/n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
 		{
-			for ((b = size - a); b > 0; b--)
+			for (b = size - a; b > 1; b--)
 			{
-				putchar(' ');
+				putchar(32);
 			}
 
-			for (b = 0; b < a; b++)
+			for (c = 0; c <= a; c++)
 			{
-				putchar('#');
+				putchar(35);
 			}
-
-			if (a == size)
-			{
-				continue;
-			}
-
 			putchar('\n');
 		}
 	}
-	putchar('\n');
 }
 
