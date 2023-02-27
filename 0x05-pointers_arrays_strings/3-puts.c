61e9c1cee81e94530096fb284a8d23566b47d5de
Lisa Powell <lisapowell46@gmail.com>
2023-02-27 18:11:42 +0200

    print string with new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  _puts - function that prints a string.
+ *  followed by a new line.
+ * @str: char to check
+ * Return: void.
+ */
+
+void _puts(char *str)
+{
+	char str[100];
+	char *n;
+
+	n = str;
+	
+	while (*str != '\0')
+	{
+		str++;
+
+	}
+	putchar('\n');
+}
