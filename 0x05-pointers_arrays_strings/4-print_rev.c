41966a55b4787a98752be6f3b7956ef1ee6a0c0f
Lisa Powell <lisapowell46@gmail.com>
2023-02-27 21:54:42 +0200

    string reversed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,17 +8,12 @@
 
 void print_rev(char *s)
 {
-	int count = 0;
+	int i = 0;
 
-	while (count >= 0)
-	{
-		if (s[count] == '\0')
-			break;
-		count++;
-	}
-	for (count--; count >= 0; count--)
-	{
-		_putchar(s[count]);
-		_putchar('\n')
-	}
+	while (s[i] != '\0')
+		i++;
+
+	for (i -= 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
