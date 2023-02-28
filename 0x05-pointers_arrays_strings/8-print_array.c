699070af4c56e327ef451d666d674d92fc8643d1
Lisa Powell <lisapowell46@gmail.com>
2023-02-28 12:01:24 +0200

    prints array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,13 +11,17 @@
 
 void print_array(int *a, int n)
 {
-int i = 0;
+int i;
 
-for (; i < n; i++)
+for (i = 0; i < n; i++)
 {
-printf("%d", *(a + 1));
 
-if (i != (n - 1))
-printf(", ");
+if (i != n - 1)
+printf("%d, ", a[i]);
+
+else
+printf("%d", *(a[i]);
+
 }
+printf("\n"0;
 }
