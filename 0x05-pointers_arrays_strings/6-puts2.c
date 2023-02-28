74c8ddcd0db0ad94575b342ae4da98b3aa32d2c1
Lisa Powell <lisapowell46@gmail.com>
2023-02-28 04:44:18 +0200

    every other char in string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other char.
+ * followed by a new line.
+ * @str: char to check
+ * Return: void.
+ */
+
+void puts2(char *str)
+{
+int n = 0;
+
+while (str[n] != '\0')
+{
+if(i % 2 == 0)
+{
+_putchar(str[n]);
+}
+i++;
+}
+putchar('\n');
+}
