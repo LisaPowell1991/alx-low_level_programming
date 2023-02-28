0a5157489154e440514e388f827beaeb155f470f
Lisa Powell <lisapowell46@gmail.com>
2023-02-28 17:44:59 +0200

    generate random password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generate random valid passwords
+ * @s: char to check
+ *
+ * Return: 0 is success
+ */
+
+int main(void)
+{
+int a = 0, b = 0;
+time_t t;
+
+//2772 is the total sum of the ASCII values of all the characters in the password, including the missing character.
+
+srand((unsigned int) time(&t));
+while (b < 2772)
+{
+a = rand() % 128;
+if ((b + a) > 2772)
+break;
+b = b + a;
+printf("%c\n", a);
+
+}
+printf("%c\n", (2772 - b));
+return (0);
+}
