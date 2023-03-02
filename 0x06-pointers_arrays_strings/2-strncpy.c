82d9e20c03146d536eed491eb052f4e664882107
LisaPowell1991 <lisapowell46@gmail.com>
2023-03-02 00:50:34 -0800

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,12 +10,11 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-		while (dest[n] != '\0')
-		{
-			dest[n] = src[n];
-			n++;
-		}
+	for (n = 0; dest[n] != '\0'; n++)
+	{
+		src[n] = dest[n];
+	}
 	src[n] = '\0';
-	
+
 	return (dest);
 }
