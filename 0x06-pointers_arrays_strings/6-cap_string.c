32bdc43d8fca4802cdd7a2d8cf959a286eaa88a7
LisaPowell1991 <lisapowell46@gmail.com>
2023-03-02 08:12:23 -0800

    capatalized all words in string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,17 +9,29 @@
 char *cap_string(char *str)
 {
 	int i = 0;
-	int space = 0;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		if ((space % 2 == 1) && str[i] >= 'a' && str[i] <= 'z')
-		{
-			str[i] = str[i] - 32;
-		}
+	while (!(str[i] >= 'a' && str[i] <= 'z'))
+		i++;
+	if (str[i - 1] == ' ' ||
+	str[i - 1] == '\t' ||
+	str[i - 1] == '\n' ||
+	str[i - 1] == ',' ||
+	str[i - 1] == ';' ||
+	str[i - 1] == '.' ||
+	str[i - 1] == '!' ||
+	str[i - 1] == '?' ||
+	str[i - 1] == '"' ||
+	str[i - 1] == '(' ||
+	str[i - 1] == ')' ||
+	str[i - 1] == '{' ||
+	str[i - 1] == '}' ||
+	 i == 0)
+		str[i] -= 32;
 
-		if (str[i] == ' ')
-			i++;
+	i++;
 	}
+
 	return (str);
 }
