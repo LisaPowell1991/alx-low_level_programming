6804bfa2322fd9af33cc80013729ff80b442ab7d
LisaPowell1991 <lisapowell46@gmail.com>
2023-03-02 01:29:00 -0800

    copies string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,10 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		src[n] = dest[n];
+		dest[i] = src[i];
 
 	for (; i < n; i++)
 		dest[i] = '\0';
 
-	_putchar('\n');
 	return (dest);
 }
