bc4fe72ef6421c8848e3758bba74d04407704401
LisaPowell1991 <lisapowell46@gmail.com>
2023-03-08 04:13:29 -0800

    length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,11 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
+	int length = 0;
 
 	if (*s != '\0')
 	{
-		return (0);
-
+		length++;
+		length += _strlen_recursion(s + 1);
 	}
-	return (1 + _strlen_recursion(s + 1));
+	return (length);
 }
