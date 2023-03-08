0b097e747ba8c8427c7737608857129b95752a11
LisaPowell1991 <lisapowell46@gmail.com>
2023-03-08 01:55:31 -0800

    print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,9 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-	if(*s)
-		_puts_recursion(s - 1);
+	if (*s == '\0')
+		_putchar('\0');
+
 	else
-		return;
-	_putchar(*s);
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+
+	}
 }
