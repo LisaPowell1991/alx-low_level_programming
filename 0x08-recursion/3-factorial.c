3ea3d5312517a14923c786d6056ce7115909e011
LisaPowell1991 <lisapowell46@gmail.com>
2023-03-08 04:57:20 -0800

    factorial of given num

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,12 +8,14 @@
 
 int factorial(int n)
 {
+	int next;
+
 	if (n == 0)
 		return (1);
 	else if (n < 0)
 		return (-1);
 
-	int next = n * factorial(n - 1);
+	next = factorial(n - 1);
 
-	return (next);
+	return (n * next);
 }
