4dcc05264bcbe04a3fad39029c67067c3521ed81
LisaPowell1991 <lisapowell46@gmail.com>
2023-03-11 09:36:36 -0800

    static library

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -9,19 +9,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-        int char_used = 0;
-        int max_size = 0;
+	int char_used = 0;
+	int max_size = 0;
 
-        while (dest[char_used] != '\0')
-        {
-                char_used++;
-        }
-        while (src[max_size] != '\0')
-        {
-                dest[char_used] = src[max_size];
-                char_used++;
-                max_size++;
-        }
-        dest[char_used] = '\0';
-        return (dest);
-}                          
+	while (dest[char_used] != '\0')
+	{
+		char_used++;
+	}
+	while (src[max_size] != '\0')
+	{
+		dest[char_used] = src[max_size];
+		char_used++;
+		max_size++;
+	}
+	dest[char_used] = '\0';
+	return (dest);
+}
