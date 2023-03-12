a2dcc03b151b57c7e2eaaebc3ba63962b777aae8
LisaPowell1991 <lisapowell46@gmail.com>
2023-03-12 03:39:08 -0700

    prints all args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -  function that prints all arguments it receives,
+ * followed by a new line.
+ * @argc:integer
+ * @argv:integer
+ * Return: returns 0.
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	int i;
+
+	printf("%d\n", argc);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
