a8881a026af2592cd52529bde16a970ab992087d
LisaPowell1991 <lisapowell46@gmail.com>
2023-03-12 05:22:43 -0700

    sum of nums

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  function that adds two positive numbers,
+ * followed by a new line.
+ * @argc:integer
+ * @argv:integer
+ * Return: returns 0 or 1 if there is not 2 arguments.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+	for (j = 0; argv[i][j]; j++)
+	{
+
+	if (argv[i][j] < '0' || argv[i][j])
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	sum = i + j;
+	}
+
+	printf("%d\n", sum);
+	}
+
+	return (0);
+}
