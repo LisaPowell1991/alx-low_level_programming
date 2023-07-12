58099f56033e49346ff8a47965f1ee51b1a0debb
LisaPowell <lisapowell46@gmail.com>
2023-07-12 05:14:31 -0700

    copies content from 1 file to another

--- 0x15-file_io/3-cp_test.c
+++ 0x15-file_io/3-cp_test.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-/**
- * displayUsageError - Displays an error message
- * Return: void
- */
+#define BUFFER_SIZE 1024
 
+/**
+ * displayUsageError - Displays an error message for incorrect command usage
+ */
 void displayUsageError(void)
 {
 	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -12,86 +12,62 @@ void displayUsageError(void)
 }
 
 /**
- * displayReadError - Displays an error message 
- * for file read failure
- * @filename: Name of the file that couldn't be read
- * Return: void
+ * copyFile - Copies the content of one file to another
+ * @source: Name of the source file
+ * @destination: Name of the destination file
  */
-
-void displayReadError(const char *filename)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-	exit(98);
-}
-
-/**
- * displayWriteError - Displays an error message 
- * for file write failure
- * @filename: Name of the file that couldn't be written
- * Return: void
- */
-
-void displayWriteError(const char *filename)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-	exit(99);
-}
-
-/**
- * displayCloseError - Displays an error message 
- * for file close failure
- * Return: void
- */
-
-void displayCloseError(void)
-{
-	dprintf(STDERR_FILENO, "Error: Can't close fd\n");
-	exit(100);
-}
-
-/**
- * copy_file - Copies the content of one file to another.
- * @source_file: The name of the source file.
- * @destination_file: The name of the destination file.
- * Return: void
- */
-
-void copy_file(const char *source_file, const char *destination_file)
+void copyFile(const char *source, const char *destination)
 {
 	int input_fd, output_fd;
+	int close_status;
 	ssize_t num_bytes_read, num_bytes_written;
-	char buffer[BUFFER_SIZE];
+	char buffer[BUFSIZ];
 
-	input_fd = open(source_file, O_RDONLY);
+	input_fd = open(source, O_RDONLY);
 	if (input_fd < 0)
-		displayReadError(source);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source);
+		exit(98);
+	}
+	output_fd = open(destination, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	output_fd = open(destination_file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (output_fd < 0)
 	{
-		close(input_fd);
-		displayWriteError(destination);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", destination);
+		exit(99);
 	}
-
-	while ((num_bytes_read = read(input_fd, buffer, BUFFER_SIZE)) > 0)
+	while ((num_bytes_read = read(input_fd, buffer, BUFSIZ)) > 0)
 	{
 		num_bytes_written = write(output_fd, buffer, num_bytes_read);
 		if (num_bytes_written != num_bytes_read)
 		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", destination);
 			close(input_fd);
 			close(output_fd);
-			displayWriteError(destination);
+			exit(99);
 		}
 	}
-
 	if (num_bytes_read < 0)
 	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source);
 		close(input_fd);
-		displayReadError(source);
+		close(output_fd);
+		exit(98);
 	}
 
-	if (close(input_fd) < 0 || close(output_fd) < 0)
-		displayCloseError();
+	close_status = close(input_fd);
+	if (close_status < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		exit(100);
+	}
+
+	close_status = close(output_fd);
+	if (close_status < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		exit(100);
+	}
 }
 
 /**
