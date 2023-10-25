b441c6bacb87cdfb934a8f09826aabd3e80bc8a9
LisaPowell1991 <lisapowell46@gmail.com>
2023-10-25 07:58:32 -0700

    a function that returns the perimeter of a island fixed pycodestyle

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,6 +2,7 @@
 
 """ A module that contains an island_perimeter definition. """
 
+
 def island_perimeter(grid):
     """
     returns the perimeter of the island described in grid:
@@ -11,7 +12,8 @@ def island_perimeter(grid):
         - One cell is a square with side length 1
         - Grid cells are connected horizontally/vertically (not diagonally).
         -Grid is rectangular, width and height don’t exceed 100
-    Grid is completely surrounded by water, and there is one island (or nothing).
+    Grid is completely surrounded by water,
+    and there is one island (or nothing).
     The island doesn’t have “lakes”
     (water inside that isn’t connected to the water around the island).
     """
